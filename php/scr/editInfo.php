79035a2c5600244c26c712faed1bfb23765a3431
Maik Riedlsperger <maik.riedlsperger@gmail.com>
2018-01-10 16:49:35 +0100

    Now all inputs get trimed
    Fixes #4

--- php/scr/editInfo.php
+++ php/scr/editInfo.php
@@ -3,7 +3,7 @@
     function editEntry($pdo, $table_name, $tables, $id){
             $sql = "UPDATE ".$table_name." SET ";
             for($i = 0; $i < sizeof($tables); $i++){
-                    $sql = $sql.$tables[$i].'="'.$_POST[$tables[$i]].'", ';
+                    $sql = $sql.$tables[$i].'="'.trim($_POST[$tables[$i]]).'", ';
             }
             $sql = substr($sql, 0, -2);	//Cut last , 
             $sql = $sql." WHERE ".$tables[0].'="'.$id.'"';
