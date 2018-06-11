9332217595697b59d67f0454737d53e656f7b284
ADarkHero <maik.riedlsperger@gmail.com>
2018-06-11 11:10:00 +0200

    htmlspecialchars are friends, not food. Fixes #9

--- php/scr/editInfo.php
+++ php/scr/editInfo.php
@@ -3,7 +3,7 @@
     function editEntry($pdo, $table_name, $tables, $id){
             $sql = "UPDATE ".$table_name." SET ";
             for($i = 0; $i < sizeof($tables); $i++){
-                    $sql = $sql.$tables[$i].'="'.trim($_POST[$tables[$i]]).'", ';
+                    $sql = $sql.$tables[$i].'="'.trim(htmlspecialchars($_POST[$tables[$i]], ENT_QUOTES)).'", ';
             }
             $sql = substr($sql, 0, -2);	//Cut last , 
             $sql = $sql." WHERE ".$tables[0].'="'.$id.'"';
